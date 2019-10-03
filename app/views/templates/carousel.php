7fa2b1a298868f41bd7d0b03ba1b927bb5d8c65a
Tawanda <tmuhwati@gmail.com>
2019-10-03 22:41:46 +0200

    Carousel update

--- app/views/templates/carousel.php
+++ app/views/templates/carousel.php
@@ -2,7 +2,7 @@
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner" role="listbox">
       <!-- Slide One - Set the background image for this slide in the line below -->
-      <div class="carousel-item active" style="background-image: url('https://ccboxing.esikolweni.co.za/resources/media/images/carousel_3.jpg')">
+      <div class="carousel-item active" style="background-image: url('https://ccboxing.esikolweni.co.za/resources/media/images/carousel_1.jpg')">
       </div>
       <!-- Slide Two - Set the background image for this slide in the line below -->
       <div class="carousel-item" style="background-image: url('https://ccboxing.esikolweni.co.za/resources/media/images/carousel_2.jpg')">
