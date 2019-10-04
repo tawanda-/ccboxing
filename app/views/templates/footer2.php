514c9e17359442cc4f393dbdaad96f0e058049c8
Tawanda <tmuhwati@gmail.com>
2019-10-04 17:12:41 +0200

    Favicon

--- app/views/templates/footer2.php
+++ app/views/templates/footer2.php
@@ -1,7 +1,27 @@
     <!-- Footer -->
-    <footer class="footer mt-auto py-3 bg-dark">
+    <footer class="footer mt-auto py-3 text-center text-small bg-dark">
         <div class="container">
-            <p class="m-0 text-center text-white">&copy; Cassius Clay Boxing <?php echo date("Y"); ?></p>
+            <p class="m-0 text-center text-muted text-white">&copy; Cassius Clay Boxing <?php echo date("Y"); ?></p>
+            <ul class="list-inline">
+                <li class="list-inline-item">
+                    <a class="text-white" href="https://ccboxing.esikolweni.co.za/home"><u>Home</u></a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="text-white" href="https://ccboxing.esikolweni.co.za/shop"><u>Store</u></a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="text-white" href="https://ccboxing.esikolweni.co.za/about"><u>About</u></a>
+                </li>
+                <li class="list-inline-item">
+                <a class="text-white" href="https://ccboxing.esikolweni.co.za/contact"><u>Contact</u></a>
+                </li>
+                <li class="list-inline-item">
+                <a class="text-white" href="https://ccboxing.esikolweni.co.za/terms"><u>T&C's</u></a>
+                </li>
+                <li class="list-inline-item">
+                <a class="text-white" href="https://ccboxing.esikolweni.co.za/login"><u>Login</u></a>
+                </li>
+            </ul>
         </div>
     <!-- /.container -->
     </footer>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,6 +5,7 @@
         </title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="https://ccboxing.esikolweni.co.za/resources/media/images/favicon.png">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="https://ccboxing.esikolweni.co.za/resources/css/ccboxing.css" >
